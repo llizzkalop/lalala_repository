456ef70d1e29149fe6d09f017eedd51e84750df3
llizzkalop <lizalopster222@gmail.com>
2023-12-28 15:47:50 +0500

    Цикл for. Элементы списка. Полезные функции в цикле

---
+++ цикл For.py
@@ -0,0 +1,14 @@
+
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 10
+for name_cars in cars:
+    print('Я езжу на автомобиле марки', name_cars)
+
+
+cars_count = 0
+for cars_count in range(10):
+    cars_count += 10
+    print(cars_count)
+
+
+
