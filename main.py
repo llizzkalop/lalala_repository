8548021fa8ae261f57fe67af5cdf7b9cbc263368
llizzkalop <lizalopster222@gmail.com>
2024-01-17 15:10:53 +0500

    Дз с модулями

---
+++ main.py
@@ -0,0 +1,2 @@
+import modules
+print('Успех!!!')
---
+++ modules.py
@@ -0,0 +1,8 @@
+print('Пробуем импорт...')
+def function1():
+    print('Hi')
+function1()
+def function2():
+    print('guys')
+function2()
+print('Импорт завершён!')
