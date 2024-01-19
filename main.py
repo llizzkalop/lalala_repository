169c7a119262d05d5c6428ab888475eb9e06fe58
llizzkalop <lizalopster222@gmail.com>
2024-01-19 13:52:34 +0500

    Исправила, чтобы функция вызывалась из main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-import modules
-print('Успех!!!')
+
+print('Пробуем импорт...')
+def function1():
+    print('Hi')
+function1()
+def function2():
+    print('guys')
+function2()
+print('Импорт завершён!')
--- modules.py
+++ modules.py
@@ -1,8 +1,2 @@
-print('Пробуем импорт...')
-def function1():
-    print('Hi')
-function1()
-def function2():
-    print('guys')
-function2()
-print('Импорт завершён!')
+import main
+print('Успех!!!')
