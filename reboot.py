41a6896e66e0edcaf77df28b65e529dd9bc0d5a8
llizzkalop <lizalopster222@gmail.com>
2024-01-30 21:13:12 +0500

    Перегрузка

---
+++ reboot.py
@@ -0,0 +1,8 @@
+class Buiding:
+    def __init__(self):
+        self.numberOfFloors= float
+        self.buildingType = str
+    def __eq__(self):
+        return self.numberOfFloors == self.buildingType
+    print('Вряд ли')
+Buiding()
