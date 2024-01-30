04255f7b1b16a222215d340529c41a16b2b80576
llizzkalop <lizalopster222@gmail.com>
2024-01-30 20:52:33 +0500

    Методы классов

---
+++ Special classes.py
@@ -0,0 +1,11 @@
+class House:
+
+    def __init__(self):
+        self.numbersOfFloors = 0
+    def setNewNumberOfFloors(self,floors):
+        self.numbersOfFloors = floors
+        print('Этаж',floors)
+floor = House
+floor.setNewNumberOfFloors(self=House,floors=3)
+
+
