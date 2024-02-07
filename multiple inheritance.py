61c049e585dd52aa93a4fc99b4451f42124a80a5
llizzkalop <lizalopster222@gmail.com>
2024-02-07 20:16:36 +0500

    Множественное наследование

---
+++ multiple inheritance.py
@@ -0,0 +1,16 @@
+class Vehicle():
+    vehicle_type = "none"
+
+class Car ():
+    price =  1000000
+    def horse_power(self):
+        self.power = n
+
+class Nissan (Vehicle, Car):
+    price = 1500000
+    vehicle_type = "Седан"
+    def horse_power(self):
+        self.power = 250
+Almera = Nissan
+print(Almera.price)
+print(Almera.vehicle_type)
